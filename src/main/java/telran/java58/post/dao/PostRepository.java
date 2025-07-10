3feb4bce082eb1dacf61bfec1c1044eb9cb5ca34
edward <edwardlfish@gmail.com>
2025-07-10 20:49:50 +0300

    post service complete

---
+++ src/main/java/telran/java58/post/dao/PostRepository.java
@@ -0,0 +1,16 @@
+package telran.java58.post.dao;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import telran.java58.post.model.Post;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Stream;
+
+public interface PostRepository extends MongoRepository<Post, String> {
+    Stream<Post> findPostsByAuthorIgnoreCase(String author);
+
+    Stream<Post> findPostsByTagsInIgnoreCase(List<String> tags);
+
+    Stream<Post> findPostsByDateCreatedBetween(LocalDate from, LocalDate to);
+}
--- src/main/java/telran/java58/post/dto/NewPostDto.java
+++ src/main/java/telran/java58/post/dto/NewPostDto.java
@@ -1,9 +1,12 @@
 package telran.java58.post.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
-
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class NewPostDto {
     private String title;
